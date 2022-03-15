56b44b2774bc264d5a26b4ca101e7342e9905e25
Quinten <31628811+puffballl@users.noreply.github.com>
2022-03-15 13:22:30 +0100

    Function for generating tables. Only need to get the data from the database into it

--- wwwroot/pad-framework/server/routes/scoreboardRoutes.js
+++ wwwroot/pad-framework/server/routes/scoreboardRoutes.js
@@ -14,12 +14,15 @@ class ScoreboardRoutes{
         this.#app.get("/scoreboard", async (req, res) => {
             try {
                 const data = await this.#databaseHelper.handleQuery({
+                    //Select all the usernames, locations and scores from the users table
                     query: "SELECT username, location, score FROM users",
                 });
 
                 //just give all data back as json, could also be empty
                 res.status(this.#errorCodes.HTTP_OK_CODE).json(data);
+
             } catch(e) {
+                //Gives an error if the request went wrong
                 res.status(this.#errorCodes.BAD_REQUEST_CODE).json({reason: e})
             }
         });
