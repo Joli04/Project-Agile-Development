9acc35c4d79545551e1b42babc6c281292a3cc6b
Joli-Coeur Weibolt <joli-coeur.weibolt@hva.nl>
2022-03-17 08:22:32 +0100

    Added pictures

--- wwwroot/pad-framework/src/assets/js/prize.js
+++ wwwroot/pad-framework/src/assets/js/prize.js
@@ -3,10 +3,13 @@ const close = document.getElementsByClassName("close")[0];
 const btn = document.getElementById("myBtn");
 const prize1 = document.getElementById('prize1');
 const prize2 = document.getElementById('prize2');
-const prize3 = document.getElementById('prize3');
+const prize3 = document.getElementById('prize3')
 const body = document.body;
 const prize_popup = document.querySelector('.popup');
 const prizes = [prize1, prize2, prize3];
+var img = document.createElement("img");
+var src = document.getElementById("popup-prize");
+
 
 function timeout() {
     setTimeout(function () {
@@ -33,16 +36,22 @@ window.onclick = function (event) {
 prize1.onmouseover = function() {
     body.className = 'hoveredp1';
     timeout();
+    img.src = "https://pbs.twimg.com/profile_images/1284476346/vakantie_reasonably_small.gif";
+    src.appendChild(img);
 }
 
 prize2.onmouseover = function (){
     body.className = 'hoveredp2';
     timeout();
+    img.src = "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/vslgdggkdt7kbg1ycqab";
+    src.appendChild(img);
 }
 
 prize3.onmouseover = function (){
     body.className = 'hoveredp3';
     timeout();
+    img.src = "https://pbs.twimg.com/profile_images/51457981/koffie_3__reasonably_small.gif";
+    src.appendChild(img);
 }
 
 for (let i = 0; i < prizes.length; i++) {
