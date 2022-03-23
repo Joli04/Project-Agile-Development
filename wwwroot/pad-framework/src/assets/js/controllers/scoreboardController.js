1c365041b8e456b55a1c60461d8b11f9e0ec0d4e
Lucas de Jonghe <lucas.de.jonghe@hva.nl>
2022-03-23 13:32:43 +0100

    Added the sort function to the controller script

--- wwwroot/pad-framework/src/assets/js/controllers/scoreboardController.js
+++ wwwroot/pad-framework/src/assets/js/controllers/scoreboardController.js
@@ -24,14 +24,56 @@ export class ScoreboardController extends Controller{
             return b.score - a.score;
         })
         ScoreboardController.#createScoreboard(labels, objects, document.getElementById('scoreboard'))
+
+        this.sortByName();
+    }
+
+    sortByName(){
+        const sortBtn = document.getElementById('sort-btn');
+
+        //change this selector to the class/id of the table
+        let compList = document.getElementById("scoreboardTbody");
+
+        let nameRow = 1;
+
+        sortBtn.addEventListener("click", () => {
+            //Chane the col selector to the column that the names are in
+            sort_table(compList, 0, nameRow); nameRow *= -1;
+        })
+
+        function sort_table(tbody, col, asc){
+            let rows = tbody.rows
+            let rlen = rows.length
+            let arr = new Array()
+
+            // fill the array with values from the table
+            for(let i = 0; i < rlen; i++){
+                let cells = rows[i].cells;
+                let clen = cells.length;
+                arr[i] = new Array();
+                for(let j = 0; j < clen; j++){
+                    arr[i][j] = cells[j].innerHTML;
+                }
+            }
+            // sort the array by the specified column number (col) and order (asc)
+            arr.sort(function(a, b){
+                return (a[col] == b[col]) ? 0 : ((a[col] > b[col]) ? asc : -1*asc);
+            });
+            // replace existing rows with new rows created from the sorted array
+            for(let i = 0; i < rlen; i++){
+                arr[i] = "<td>"+arr[i].join("</td><td>")+"</td>";
+            }
+            tbody.innerHTML = "<tr>"+arr.join("</tr><tr>")+"</tr>";
+        }
     }
 
     static #createScoreboard(labels, objects, container) {
         let table = document.createElement('table');
         let thead = document.createElement('thead');
         let tbody = document.createElement('tbody');
+        tbody.setAttribute('id','scoreboardTbody');
 
-        tbody.setAttribute('id', 'scoreboard');
+        let tbodyTd;
 
         table.classList.add("table");
         thead.classList.add("tablehead");
@@ -49,7 +91,7 @@ export class ScoreboardController extends Controller{
         for (let j = 0; j < objects.length; j++) {
             let tbodyTr = document.createElement('tr')
             for (let k = 0; k < labels.length; k++) {
-                let tbodyTd = document.createElement('td')
+                tbodyTd = document.createElement('td')
                 tbodyTd.innerHTML = objects[j][labels[k].toLowerCase()]
                 tbodyTr.appendChild(tbodyTd);
             }
@@ -57,11 +99,7 @@ export class ScoreboardController extends Controller{
         }
         table.appendChild(tbody);
         container.appendChild(table);
-        tbodytd.classList.add("tbodytd");
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "../features/sortByName.js";
-        container.appendChild(script);
+        tbodyTd.classList.add("tbodytd");
     }
 
 }
